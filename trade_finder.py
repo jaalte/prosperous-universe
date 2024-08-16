771f6d2f336f64287288d046d9c68ac4629b9475
jaalte <jaalte@proton.me>
2024-08-16 02:50:48 -0700

    Completed trade_finder.py initial working version

--- trade_finder.py
+++ trade_finder.py
@@ -4,30 +4,17 @@ import json
 import math
 from fio_api import fio
 import fio_utils as utils
+from pathfinding import jump_distance
 
-origin = "NC1"
-travel_cost = 5000
+origin = "AI1" # IC1
+base_cost = 1000
+cost_per_jump = 750
+liquid_assets = 40000
 
 ship_specs = {
     "weight": 500,
     "volume": 500,
 }
-
-""" Example good dict:
-{
-  "MaterialTicker": "AMM",
-  "ExchangeCode": "NC1",
-  "MMBuy": 61.0,
-  "MMSell": null,
-  "PriceAverage": 221.66,
-  "AskCount": 6576,
-  "Ask": 222.0,
-  "Supply": 11518,
-  "BidCount": 470,
-  "Bid": 200.0,
-  "Demand": 39642
-}
-"""
 
 def main():
     exchanges = utils.get_all_exchanges()
@@ -35,22 +22,24 @@ def main():
     allmaterials = fio.request("GET", "/material/allmaterials", cache=60*60*24)
     material_lookup = {material['Ticker']: material for material in allmaterials}
 
-    #for code, exchange in exchanges.items():
-        #print(f"{code}: {exchange.name}")
+    oex = exchanges[origin]
+    destinations = {}
+    for code, exchange in exchanges.items():
+        if code == origin: continue
+        dex = exchange
+
+        dex.distance = jump_distance(oex.system_natural_id, dex.system_natural_id)
+        destinations[dex.ticker] = dex
+        #print(f"{code}: {dex.name} at distance {dex.distance}")
 
 
     # First pass: filter to only profitable routes
-    profitable_routes = []
-    # Iterate through other exchanges
-    for dest_code, exchange in exchanges.items():
-        if dest_code == origin: continue
-        oex = exchanges[origin]
-        dex = exchanges[dest_code]
-
+    for code, dex in destinations.items():
+        dex.profitable_routes = []
         # Check all materials
         for material_ticker in exchanges[origin].goods:
-            og = exchanges[origin].goods[material_ticker]
-            dg = exchanges[dest_code].goods[material_ticker]
+            og = oex.goods[material_ticker]
+            dg = dex.goods[material_ticker]
             
             if og['Ask'] is None or og['Ask'] == 0: continue
             if dg['Bid'] is None or dg['Bid'] == 0: continue
@@ -60,27 +49,127 @@ def main():
                 "destination": dex,
                 "material": material_ticker,
                 "profit_per_unit": dg['Bid'] - og['Ask'],
+                "profit_ratio": dg['Bid'] / og['Ask'],
                 "origin_good": og,
                 "destination_good": dg,
             }
 
             if route['profit_per_unit'] > 0:
-                profitable_routes.append(route)
-    
-
-    #for route in profitable_routes:
-
-    
-
-    print(f"{route['origin'].ticker} -> {route['destination'].ticker}: {route['material']}:")
-    print(f"    {route['profit_per_unit']:.2f}: {route['origin_good']['Ask']} -> {route['destination_good']['Bid']}")
+                dex.profitable_routes.append(route)
 
     # Optional: Check actual trades available to accurately tally bid/ask costs
      # Good to do now since far fewer viable routes
 
-    # Sort in order of profit and group by destination
+    # Sort profitable routes by decreasing profit
+    for code, dex in destinations.items():
+        dex.profitable_routes.sort(key=lambda x: x['profit_ratio'], reverse=True)
 
-    # For each destination
+    #print(json.dumps(destinations['IC1'].profitable_routes, indent=4))
+
+    for code, dex in destinations.items():
+        trades = []
+        for route in dex.profitable_routes:
+            material = material_lookup[route['material']]
+
+            buyable_orders = route['origin_good']['SellingOrders']
+            sellable_orders = route['destination_good']['BuyingOrders']
+
+            # Sort buyable orders from least to most expensive
+            buyable_orders.sort(key=lambda x: x['ItemCost'])
+            
+            # Sort sellable orders from most to least expensive
+            sellable_orders.sort(key=lambda x: x['ItemCost'], reverse=True)
+
+            for buyable_order in buyable_orders:
+                buyable_order['material'] = route['material']
+
+            for sellable_order in sellable_orders:
+                sellable_order['material'] = route['material']
+
+            # loop until either is empty
+            while len(buyable_orders) > 0 and len(sellable_orders) > 0:
+                if buyable_orders[0]['ItemCost'] >= sellable_orders[0]['ItemCost']: break
+
+                amount = min(buyable_orders[0]['ItemCount'], sellable_orders[0]['ItemCount'])
+                profit_per_unit = sellable_orders[0]['ItemCost'] - buyable_orders[0]['ItemCost']
+                profit_ratio = sellable_orders[0]['ItemCost'] / buyable_orders[0]['ItemCost']
+
+                trades.append({
+                    'buy': buyable_orders[0].copy(),  # copy to avoid reference issues
+                    'sell': sellable_orders[0].copy(),  # copy to avoid reference issues
+                    'material': route['material'],
+                    'amount': amount,
+                    'profit_per_unit': profit_per_unit,
+                    'profit_ratio': profit_ratio,
+                })
+
+                if buyable_orders[0]['ItemCount'] > sellable_orders[0]['ItemCount']:
+                    buyable_orders[0]['ItemCount'] -= amount
+                    sellable_orders.pop(0)
+                elif buyable_orders[0]['ItemCount'] < sellable_orders[0]['ItemCount']:
+                    sellable_orders[0]['ItemCount'] -= amount
+                    buyable_orders.pop(0)
+                else:
+                    buyable_orders.pop(0)
+                    sellable_orders.pop(0)
+
+                while len(buyable_orders) > 0 and buyable_orders[0]['ItemCount'] == None: buyable_orders.pop(0)
+                while len(sellable_orders) > 0 and sellable_orders[0]['ItemCount'] == None: sellable_orders.pop(0)
+
+        # sort trades by profit ratio in descending order
+        trades.sort(key=lambda x: x['profit_ratio'], reverse=True)
+        #print(json.dumps(trades, indent=4))
+
+        remaining_volume = ship_specs['volume']
+        remaining_weight = ship_specs['weight']
+        remaining_credits = liquid_assets
+        approved_trades = []
+
+        for trade in trades:
+            material = material_lookup[trade['buy']['material']]
+
+            max_units = get_max_space_remaining(trade, material, remaining_weight, remaining_volume, remaining_credits)
+
+            if max_units < trade['amount']:
+                # Trade is too large, only take what can fit
+                trade['amount'] = max_units
+            
+            remaining_volume -= trade['amount'] * material['Volume']
+            remaining_weight -= trade['amount'] * material['Weight']
+            remaining_credits -= trade['amount'] * trade['buy']['ItemCost']
+            
+            if trade['amount'] > 0:
+                approved_trades.append(trade)
+        
+        
+        cost = liquid_assets - remaining_credits
+        weight = ship_specs['weight']
+        volume = ship_specs['volume']
+
+        total_profit = 0
+        for trade in approved_trades:
+            total_profit += trade['profit_per_unit'] * trade['amount']
+        
+        adjusted_profit = total_profit - get_fuel_cost(dex.distance)
+
+        if adjusted_profit > 0:
+            print(f"{dex.ticker}: {adjusted_profit:.2f} profit, (Requires {cost:.2f} NCC, {weight:.2f} kg, {volume:.2f} m3)")
+            for trade in approved_trades:
+                print(f"{trade['amount']:>5} {trade['material']:<3}: {trade['sell']['ItemCost']:.2f} - {trade['buy']['ItemCost']:.2f} -> {trade['profit_per_unit']:.2f} ({trade['profit_ratio']*100:.2f}%)")
+            print()
+
+        
+
+def get_max_space_remaining(trade, material, remaining_weight, remaining_volume, remaining_credits):
+    max_by_volume = int(remaining_volume / material['Volume'])
+    max_by_weight = int(remaining_weight / material['Weight'])
+    max_by_cost   = int(remaining_credits / trade['buy']['ItemCost'])
+    max_units = min(max_by_volume, max_by_weight, max_by_cost)
+    return max_units
+
+
+def get_fuel_cost(jumps):
+    return base_cost + cost_per_jump * jumps
 
 if __name__ == "__main__":
     main()
