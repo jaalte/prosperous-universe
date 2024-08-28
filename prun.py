ec8e00c16867ddb8e03aefe759a8ebda83d10783
jaalte <jaalte@proton.me>
2024-08-27 17:44:17 -0700

    Finally committed prun.py stub

---
+++ prun.py
@@ -0,0 +1,5 @@
+# Python module for gathering FIO data
+
+#from fio_api import fio
+# Rather than importing fio, include it in this module
+# Scripts can use prun.fio.init(api_key) to set the API key
