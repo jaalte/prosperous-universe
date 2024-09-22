f501120e75ac0876948fb78322713be2ccc75316
jaalte <jaalte@proton.me>
2024-09-22 12:44:59 -0700

    Updated test.sh to reflect current working utilities

--- test.sh
+++ test.sh
@@ -1,7 +1,9 @@
 # python fio_api.py
-python fio_utils.py
-python planet_ranker.py
-python trade_finder.py
+#python fio_utils.py
+python planet-ranker.py
+python trade-finder.py
 python calc.py 10 BSE 15 FE 12 H2O 15 jal
+python planet-search.py search LST
+python manufacture-ranker.py Montem
 #python liquid_finder.py
 #python analyze.py
