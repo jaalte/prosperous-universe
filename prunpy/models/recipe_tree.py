bfcc3954018d4a01573dce9ab715f2c0d2bc625d
jaalte <jaalte@proton.me>
2024-09-22 20:31:25 -0700

    Implemented terminal materials

--- prunpy/models/recipe_tree.py
+++ prunpy/models/recipe_tree.py
@@ -1,16 +1,21 @@
 from prunpy.data_loader import loader
 
 class RecipeTreeNode:
-    def __init__(self, recipe, depth=0, multiplier=1, priority_mode='profit_ratio', include_worker_upkeep=False):
+    def __init__(self, recipe, depth=0, multiplier=1, priority_mode='profit_ratio', include_worker_upkeep=False, terminals=[]):
         self.recipe = recipe
         self.depth = depth
         self.multiplier = multiplier
         self.priority_mode = priority_mode
         self.include_worker_upkeep = include_worker_upkeep
+        self.terminals = terminals
 
         self.children = {}
 
         for input_ticker in self.recipe.inputs.tickers:
+            if input_ticker in self.terminals:
+                print(f"Skipping {input_ticker}")
+                continue
+
             #print(input_ticker)
             recipes = loader.get_material_recipes(input_ticker, 
                 include_mining_from_planet_id='XG-326a',
@@ -32,8 +37,9 @@ class RecipeTreeNode:
                     depth=self.depth + 1,
                     multiplier=new_child_multiplier,
                     priority_mode=self.priority_mode,
-                    include_worker_upkeep=self.include_worker_upkeep
-                )
+                    include_worker_upkeep=self.include_worker_upkeep,
+                    terminals = self.terminals
+                )*new_child_multiplier
 
     def __mul__(self, multiplier):
         if not isinstance(multiplier, int) and not isinstance(multiplier, float):
@@ -44,20 +50,32 @@ class RecipeTreeNode:
         newself.recipe.duration *= multiplier
         newself.recipe.inputs *= multiplier
         newself.recipe.outputs *= multiplier
-        newself.recipe.multiplier = newself.recipe.multiplier or 1
-        newself.recipe.multiplier *= multiplier
+        if newself.multiplier:
+            newself.multiplier *= multiplier
+        else:
+            newself.multiplier = multiplier
 
         return newself
 
     def __rmul__(self, multiplier):
         return self.__mul__(multiplier)
 
+    def copy(self):
+        return RecipeTreeNode(
+            recipe=self.recipe.copy(),
+            depth=self.depth,
+            multiplier=self.multiplier,
+            priority_mode=self.priority_mode,
+            include_worker_upkeep=self.include_worker_upkeep,
+            terminals = self.terminals
+        )
+
     @property
     def has_children(self):
         return len(self.children) > 0
 
     @property
-    def isterminal(self):
+    def is_terminal(self):
         return not self.has_children
 
     def sort_recipes(self, recipes, priority_mode):
@@ -70,6 +88,25 @@ class RecipeTreeNode:
         else:
             raise ValueError(f"Invalid priority mode: {priority_mode}")
 
+    @property
+    def building_days(self):
+        return self.recipe.duration*self.multiplier / 24
+
+    def get_total_building_days(self):
+        total = {}
+        if self.recipe.building in total.keys():
+            total[self.recipe.building] += self.building_days
+        else:
+            total[self.recipe.building] = self.building_days
+        for child in self.children.values():
+            child_building_days = child.get_total_building_days()
+            for building, days in child_building_days.items():
+                if building in total.keys():
+                    total[building] += days
+                else:
+                    total[building] = days
+        return total
+    
     def get_total_inputs(self, include_worker_upkeep=False):
         inputs = self.recipe.inputs * self.multiplier
         for child in self.children:
@@ -81,6 +118,9 @@ class RecipeTreeNode:
         for child in self.children:
             outputs += child.get_total_outputs(include_worker_upkeep)
         return outputs
+
+    #def get_total_buildings(self):
+        #own_building = 
 
     def __str__(self):
         string = "| " * self.depth + str(self.recipe)
