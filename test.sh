b6748347c07c89a70e043cd986cdcb1151183fe1
jaalte <jaalte@proton.me>
2024-10-14 02:56:53 -0700

    Removed trade-finder as it takes way too long to gather data

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 # python fio_api.py
 #python fio_utils.py
 python planet-ranker.py
-python trade-finder.py
+#python trade-finder.py
 python calc.py 10 BSE 15 FE 12 H2O 15 jal
 python planet-search.py search LST
 python manufacture-ranker.py Montem
